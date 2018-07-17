c8fb2353e1a4c29f142f3834c7f19d9d39535a52
nagyrobert1990 <nagy.robert1990@gmail.com>
2018-07-17 10:20:38 +0200

    Add project files.

---
+++ SerializePeople/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace SerializePeople
+{
+    [Serializable()]
+    class Person 
+    {
+        public String Name { get; }
+        public DateTime BirthDate { get; set; }
+        public enum Genders { Male, Female };
+        public Genders Gender { get; set; }
+        public int Age { get; }
+        
+
+        public Person(String name, DateTime birthDate, Genders gender)
+        {
+            this.Name = name;
+            this.BirthDate = birthDate;
+            this.Gender = gender;
+            this.Age = DateTime.Today.Year - this.BirthDate.Year;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name} BirthDate: {BirthDate.Year} {BirthDate.Month} {BirthDate.Day} Sex: {Gender}";
+        }
+    }
+}
---
+++ SerializePeople/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializePeople
+{
+    class Program
+    {
+        public enum Genders { Male, Female };
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
